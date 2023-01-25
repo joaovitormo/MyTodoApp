4af13b8f70b10b0e4339dff2a5f62af88365ae53
Mende <Mende@DESKTOP>
2023-01-25 18:53:56 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyTodoApp.Data;
+
+//banco de dados em memoria
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    //representa a tabela
+    public DbSet<Models.Todo> Todos { get; set; }  
+}
